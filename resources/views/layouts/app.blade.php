e2eb042543cbac9fe2793b3c30550c4513b0b052
Segun Afolarin <afolarinoluwasegun811@gmail.com>
2026-04-19 09:38:14 +0100

    yes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? config('app.name') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 
     <style>
         .logo-float {
